b0e1e75cec18cffd85a6530edca41e19456ef673
Pavel Demidovich <p.demidovich@softmax.by>
2022-08-11 17:08:26 +0300

    initial project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <h1>index page</h1>
+    </div>
+  )
+}
