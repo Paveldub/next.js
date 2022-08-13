6c8412b99effe6ccc93cda5fe3d8af08458b293c
Pavel Demidovich <p.demidovich@softmax.by>
2022-08-13 16:25:19 +0300

    basic project styles/variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from "next/app"
+import Head from "next/head"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My Next App</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
