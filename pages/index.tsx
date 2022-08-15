a5c43edc3ce6209aeed44346bf34f801f187d3b2
Pavel Demidovich <p.demidovich@softmax.by>
2022-08-15 15:47:45 +0300

    Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from "../components"
+import { Button, Htag } from "../components"
 
 export default function Home() {
   return (
     <div>
-      <Htag tag="h1">Text</Htag>
+      <Htag tag="h1">index page</Htag>
+      <Button appearance="primary">test button</Button>
+      <Button appearance="ghost">test button</Button>
     </div>
   )
 }
