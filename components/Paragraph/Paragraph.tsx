ad0def5e3893c3a3792a43ab9d49331f7eea57cf
Pavel Demidovich <p.demidovich@softmax.by>
2022-08-15 18:07:11 +0300

    icons + paragrap component and button component

---
+++ components/Paragraph/Paragraph.tsx
@@ -0,0 +1,25 @@
+import { ParagraphProps } from "./Paragraph.props"
+import styles from "./Paragraph.module.css"
+import cn from "classnames"
+
+export const Paragraph = ({
+  children,
+  size = "m",
+  className,
+  ...props
+}: ParagraphProps): JSX.Element => {
+  return (
+    <>
+      <p
+        className={cn(styles.p, className, {
+          [styles.s]: size == "s",
+          [styles.m]: size == "m",
+          [styles.l]: size == "l",
+        })}
+        {...props}
+      >
+        {children}
+      </p>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
-import { Button, Htag } from "../components"
+import { Button, Htag, Paragraph } from "../components"
 
 export default function Home() {
   return (
     <div>
       <Htag tag="h1">index page</Htag>
-      <Button appearance="primary">test button</Button>
-      <Button appearance="ghost">test button</Button>
+      <Button appearance="primary" arrow="right">
+        test button
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        test button
+      </Button>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat omnis
+        esse asperiores architecto dolore quasi quae praesentium velit quis, ex,
+        nisi modi cumque numquam sequi, natus harum incidunt impedit nemo!
+      </Paragraph>
     </div>
   )
 }
