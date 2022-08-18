6476d5ed97b365eb857fc348d180ea374c6aa4c2
Pavel Demidovich <p.demidovich@softmax.by>
2022-08-18 17:12:28 +0300

    footer component

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,22 @@
 import styles from "./Footer.module.css"
 import cn from "classnames"
 import { FooterProps } from "./Footer.props"
+import { format } from "date-fns"
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
   return (
     <>
-      <div {...props}>footer</div>
+      <footer {...props} className={cn(className, styles.footer)}>
+        <div>
+          OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены
+        </div>
+        <div>
+          <a href="">Пользовательское соглашение</a>
+        </div>
+        <div>
+          <a href="">Политика конфиденциальности</a>
+        </div>
+      </footer>
     </>
   )
 }
