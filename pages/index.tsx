6250eea3f9b8ec2a36ab145cd533ac458edb2067
Pavel Demidovich <p.demidovich@softmax.by>
2022-08-19 16:47:43 +0300

    getStatic props for requests & getStaticPaths for routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from "next"
 import { useState } from "react"
 import { Button, Htag, Paragraph, Rating, Tags } from "../components"
 import { withLayout } from "../layout/Layout"
+import axios from "axios"
+import { MenuItem } from "../interfaces/menu.interface"
 
-function Home() {
+function Home({ menu }: HomeProps) {
   const [rating, setRating] = useState<number>(4)
 
   return (
@@ -26,8 +29,33 @@ function Home() {
       </Tags>
       <div>-------</div>
       <Rating rating={rating} isEditable={true} setRating={setRating} />
+      <ul>
+        {menu.map((menu) => (
+          <li key={menu._id.secondCategory}>{menu._id.secondCategory}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    { firstCategory }
+  )
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[]
+  firstCategory: number
+}
